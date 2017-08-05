dffdc034a8b0f8d8e7a782dc96fd1d3fe1aa5ee3
Nyan Win <promise286@gmail.com>
2017-08-05 15:41:21 +0630

    It is javascript

--- src/Condition.js
+++ src/Condition.js
@@ -1,74 +1,82 @@
 import React, { Component } from "react";
 
 export default class Condition extends Component {
+  nextComponent($components, $current) {
+    let childrens = $components;
+    return childrens[$current + 1] ? childrens[$current + 1] : null;
+  }
 
-    nextComponent($components, $current) {
-        let childrens = $components;
-        return childrens[$current+1] ? childrens[$current+1] : null;
+  prevComponent($components, $current) {
+    let childrens = $components;
+    return childrens[$current - 1] ? childrens[$current - 1] : null;
+  }
+
+  render() {
+    let childrens = React.Children.map(this.props.children, child => child);
+    let renderable;
+
+    var myMap = new Map();
+    childrens.map((child, key) => {
+      let current = myMap.get(child.type.name);
+      let value = current === undefined ? 1 : current + 1;
+      myMap.set(child.type.name, value);
+    });
+
+    if (myMap.get("If") > 1) {
+      throw new Error(
+        `If should not appear ${myMap.get("If")} times in Condition Block`
+      );
     }
 
-    prevComponent($components, $current) {
-        let childrens = $components;
-        return childrens[$current-1] ? childrens[$current-1] : null;
+    if (myMap.get("Else") > 1) {
+      throw new Error(
+        `Else should not appear ${myMap.get("ElseIf")} times in Condition Block`
+      );
     }
 
-    render() {
-        let childrens = React.Children.map(this.props.children, child => child);
-        let renderable;
+    childrens.map((child, key) => {
+      if (child.type.name === "Else") {
+        let prev = this.prevComponent(childrens, key);
+        let next = this.nextComponent(childrens, key);
 
-        var myMap = new Map();
-        childrens.map((child, key) => {
-            let current = myMap.get(child.type.name);
-            let value = ( current === undefined ? 1 : current+1 );
-            myMap.set(child.type.name, value)
-        })
-
-        if (myMap.get('If') > 1) {
-            throw new Error(`If should not appear ${myMap.get('If')} times in Condition Block`);
+        if (prev === null) {
+          throw new Error("Else's previous condition should be If or ElseIf");
         }
 
-        if (myMap.get('Else') > 1) {
-            throw new Error(`Else should not appear ${myMap.get('ElseIf')} times in Condition Block`);
+        if (next !== null) {
+          throw new Error("Else's should not have next condition");
         }
+      }
+      if (child.type.name === "If") {
+        let next = this.nextComponent(childrens, key);
+        if (next === "If") {
+          throw new Error("If's next item must not be If in Condition Block");
+        }
+      } else if (child.type.name === "ElseIf") {
+        let next = this.nextComponent(childrens, key);
+        if (next === "If") {
+          throw new Error(
+            "ElseIf's next item must not be If in Condition Block"
+          );
+        }
+      }
+    });
 
-        childrens.map((child , key)=> {
-            if(child.type.name === 'Else'){
-                let prev = this.prevComponent(childrens, key);
-                let next = this.nextComponent(childrens, key);
+    childrens.some(child => {
+      if (
+        child.props.is === true ||
+        child.props.is === 1 ||
+        child.type.name === "Else"
+      ) {
+        renderable = child;
+        return true;
+      }
+    });
 
-                if (prev === null) {
-                    throw new Error('Else\'s previous condition should be If or ElseIf');
-                } 
-
-                if (next!==null) {
-                    throw new Error('Else\'s should not have next condition');
-                }
-            }
-            if(child.type.name === 'If'){
-                let next = this.nextComponent(childrens, key);
-                if (next === 'If') {
-                    throw new Error('If\'s next item must not be If in Condition Block');
-                }
-            }
-            else if(child.type.name === 'ElseIf'){
-                let next = this.nextComponent(childrens, key);
-                if (next === 'If') {
-                    throw new Error('ElseIf\'s next item must not be If in Condition Block');
-                }
-            }
-        });
-
-        childrens.some(child => {
-            if(child.props.is === true || child.props.is === 1 || child.type.name === 'Else'){
-                renderable = child;
-                return true;
-            }
-        });
-
-        return (
-            <div>
-                {renderable}
-            </div>
-        );
-    }
+    return (
+      <div>
+        {renderable}
+      </div>
+    );
+  }
 }
